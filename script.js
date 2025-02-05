a332062d7da7b6984e39fbc4ebdbab12e401edbc
Mara1ik <pechenevmarat@gmail.com>
2025-02-05 22:12:33 +0300

    adapt header, useful for mobile

--- script.js
+++ script.js
@@ -50,3 +50,47 @@ form.addEventListener("submit", (e) => {
     form.submit();
   }
 });
+
+const burgerBut = document.querySelector(".header__burger");
+const crossBut = document.querySelector(".navbar__cross");
+
+const navBar = document.querySelector(".navbar");
+const header = document.querySelector(".header");
+const headerContainer = header.querySelector(".header__container");
+const useful = document.querySelector(".useful");
+const usefulContainer = useful.querySelector(".useful__container");
+
+const experienceLink = document.querySelector("#experienceLink");
+const contactLink = document.querySelector("#contactLink");
+
+const openNavBar = () => {
+  navBar.style.display = "block";
+  header.style.opacity = 0.25;
+  headerContainer.style.border = "none";
+  useful.style.opacity = 0.25;
+  usefulContainer.style.border = "none";
+};
+
+const closeNavBar = () => {
+  navBar.style.display = "none";
+  header.style.opacity = 1;
+  headerContainer.style.cssText =
+    "border: 1rem solid #000; border-bottom: none;";
+  useful.style.opacity = 1;
+  usefulContainer.style.cssText =
+    "border: 1rem solid #000; border-top: none;  border-bottom: none;";
+};
+
+const scroll = (height) => {
+  closeNavBar();
+  window.scrollTo(0, height / (1920 / window.innerWidth));
+};
+
+burgerBut.addEventListener("click", openNavBar);
+crossBut.addEventListener("click", closeNavBar);
+experienceLink.addEventListener("click", () => {
+  scroll(800);
+});
+contactLink.addEventListener("click", () => {
+  scroll(1600);
+});
